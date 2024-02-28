fd4da668a814b56504a3f359eb87175f3cd9120f
Kerlous Aweeda <111410896+Kerlousaweeda0730@users.noreply.github.com>
2024-02-28 12:49:01 -0500

    Delete frontend directory

--- frontend/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "./styles.css";
-import {App} from "./components/App"
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.querySelector("#root"));
-root.render(<App/>)
